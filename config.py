95b027210aa9e226fb3460e5e83a895779b6aa9c
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2024-03-01 07:20:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 OWNER_ID = os.getenv("OWNER_ID",1238234357)
-HANDLER = ["~", ".","!","?","@","$"]
-LOVELY = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
+HANDLER = [".","!","@"]
+LOVELY = "https://te.legra.ph/file/794a9a75ae32566d41853.mp4"
 GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
-SOURCE = "https://github.com/TeamX-Coders/PyroX-V2"
+SOURCE = "https://github.com/Darklightning2008/LovelyX"
 LOG_CHAT = -1002119409366
